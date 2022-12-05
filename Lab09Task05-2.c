bb525fc628609a72443b32a6efc2fcdb02d39cfb
ArthurCaeorem <65918714+ArthurCaeorem@users.noreply.github.com>
2022-12-04 20:34:50 -0500

    Update Lab09Task05-2.c

--- Lab09Task05-2.c
+++ Lab09Task05-2.c
@@ -1,1 +1,24 @@
-
+#include<stdio.h>
+#include<sys/shm.h>
+#include<errno.h>
+#include<stdlib.h>
+#include "header.h"
+/*
+This program erases shared memory of key x inside current working directory.
+*/
+int main() {
+key_t key;
+int shmid;
+struct Memory shm;
+//gets key
+key = ftok(".", 'x');
+//accesses shared memory from key
+if ((shmid = shmget(key, sizeof(struct Memory), 0)) <0)
+{
+perror("shmget error \n");
+exit (1);
+}
+//removes the shared memory
+shmctl(shmid, IPC_RMID, NULL);
+exit(0);
+}
